98505fde07411c08968ba50925577529ad363f21
AL <allanlin666666@gmail.com>
2021-07-02 16:29:17 -0700

    Finished dataset code, tested.

--- data/dataset.py
+++ data/dataset.py
@@ -2,6 +2,7 @@
 import json
 import os
 import random
+import math
 
 import numpy as np
 import torch
@@ -12,10 +13,90 @@ from torch.utils.data import Dataset
 from PIL import Image
 from pycocotools import mask as coco_mask
 from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
+
+# %%
+def format_json(json_file):
+    """Formats coco json by grouping features by their image_ids"""
+    with open(json_file, 'r') as f:
+        annote = json.load(f)
+    
+    image_ids = {}  # key = image_ids, item = nested dict classes and their bounding boxes
+    for i in annote['annotations']:
+        if i['image_id'] in image_ids:
+            if i['category_id'] in image_ids[i['image_id']]:
+                image_ids[i['image_id']][i['category_id']].append(i['bbox'])
+            else:
+                image_ids[i['image_id']].update({i['category_id']: [i['bbox']]})
+        else:
+            image_ids[i['image_id']] = {i['category_id']: [i['bbox']]}
+    return image_ids
 
 
+def fetch_query(image, bbox):
+    """
+    Fetches query images, alias to cropping
+    Args:
+        image: str or PIL.Image
+        bbox: coco annotation format; x, y of top most point, x right, y down from that point
+    """
+    if type(image) == str:
+        image = Image.open(image, mode='RGB')
+    return F.crop(image, bbox[1], bbox[0], bbox[3], bbox[2])
+
+
+def id_to_file(id: str, image_dir) -> str:
+    """image_id to file name"""
+    id = str(id)
+    file = '0' * (12 - len(id)) + id + '.jpg'
+    return image_dir + '/' + file
+
+
+def make_query_pool(image_dir, json_file, name):
+    if not os.path.exists(name):
+        os.mkdir(name)
+    image_ids = format_json(json_file)
+    classes_counter = {}
+    for id in image_ids:
+        file = id_to_file(id, image_dir)
+        img = Image.open(file)
+        for class_id in image_ids[id]:
+            path = name + '/' + str(class_id)
+            if not os.path.exists(path):
+                os.mkdir(path)
+                classes_counter[int(class_id)] = 0
+            for bbox in image_ids[id][class_id]:
+                query: Image.Image = fetch_query(img, bbox)
+                query.save(path + '/' + str(classes_counter[class_id]) + '.jpg')
+                classes_counter[class_id] += 1
+    with open(name + '/instances.json', 'w') as f:
+        json.dump(classes_counter, f)
+    print('done')
+
+
+def plot_box(image: np.ndarray, bbox) -> None:
+    bbox = np.asarray(bbox)
+    plt.imshow(image)
+    ax = plt.gca()
+    if len(bbox.shape) == 1:
+        rect = Rectangle(bbox[:2], bbox[2], bbox[3], linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+    else:
+        for box in bbox:
+            rect = Rectangle(box[:2], box[2], box[3], linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+    plt.show()
+
+
+def visualize_coco(i, q, b):
+    plot_box(i, b)
+    for i in q:
+        plt.imshow(i)
+        plt.show()
+
+# %%
 class CocoBoxes(Dataset):
-    def __init__(self, image_dir: str, json_file: str, query_transforms=None, transforms=None, case4: bool=False, case4_dir: str=None) -> None:
+    def __init__(self, image_dir: str, json_file: str, query_transforms=None, transforms=None, query_pool: str=None, case4_prob=0.5, case4_sigma=3) -> None:
         """
         Dataset class which gives bounding boxes, query images respective input images
         for arbitrary bounding box detection following four cases.
@@ -33,37 +114,32 @@ class CocoBoxes(Dataset):
         boxes needs to have a direct matching to one of the query images.
 
         Args:
-            case4: whether to activative case4 or not, if True, all query images must be written to disk for efficiency;
-                   a new directory must be constructed.
-            case4_dir: if case4 is True, the location of such a directory
+            query_pool: whether to activative case4 or not, if not None, indicates the directory of the query
+                        pool, which is constructed by the make_query_pool function.
+            
+            case4_prob: the probability of entering case4 if query_pool is not None.
+
+            case4_sigma: the stddev of the amount of images to draw from the query pool
         """
 
         self.image_dir = image_dir
         self.json_file = json_file
         self.transforms = transforms
         self.query_transforms = query_transforms
-        self.case4 = case4
+        self.query_pool = query_pool
+        self.case4_prob = case4_prob
+        self.case4_sigma = case4_sigma
 
-        with open(self.json_file, 'r') as f:
-            annote = json.load(f)
-        
-        self.image_ids = {}  # key = image_ids, item = nested dict classes and their bounding boxes
-        for i in annote['annotations']:
-            if i['image_id'] in self.image_ids:
-                if i['category_id'] in self.image_ids[i['image_id']]:
-                    self.image_ids[i['image_id']][i['category_id']].append(i['bbox'])
-                else:
-                    self.image_ids[i['image_id']].update({i['category_id']: [i['bbox']]})
-            else:
-                self.image_ids[i['image_id']] = {i['category_id']: [i['bbox']]}
+        self.image_ids = format_json(json_file)
         self.keys = list(self.image_ids.keys())
-        del annote
 
-    def id_to_file(self, id: str) -> str:
-        """image_id to file name"""
-        id = str(id)
-        file = '0' * (12 - len(id)) + id + '.jpg'
-        return self.image_dir + '/' + file
+        if query_pool is not None:
+            with open(os.path.join(query_pool, 'instances.json'), 'r') as f:
+                # query instances describes range of file names under each class
+                # to prevent loading all file paths into memory
+                self.query_instances = json.load(f)
+        
+        print('{json_file} loaded into memory.')
     
     def random_query_bbox(self, bboxes: np.ndarray):
         """
@@ -71,19 +147,28 @@ class CocoBoxes(Dataset):
         """
         q = len(bboxes)
         indx = np.random.permutation(q)
-        q_stop = np.random.randint(low=1, high=q, size=1)
-        b_stop = np.random.randint(low=q_stop, high=q, size=1)
+        q_stop = random.randint(1, q)
+        b_stop = random.randint(q_stop, q)
         return bboxes[indx[:q_stop]], bboxes[indx[:b_stop]]  # query images, bboxes; query images <= bboxes
     
-    def fetch_query(self, image, bbox):
+    def random_query(self, class_id: int, sigma: int=4.0) -> Image.Image:
         """
-        bbox = x, y of top most point, x right, y down from that point
+        Draws queries from the class, the amount of which follows
+        a normal distribution with mean 1 and stddev 4
         """
+        size = random.normalvariate(1.0, sigma)
+        size = math.floor(abs(size)) + 1  # make sure that size is not 0
+        folder_path = os.path.join(self.query_pool, str(class_id))
+        queries = []
+        indx = 0
+        while indx < size:
+            f = random.randint(0, self.query_instances[str(class_id)])
+            f = folder_path + '/' + str(f) + '.jpg'
+            if f not in queries:
+                queries.append(f)
+                indx += 1
+        return [Image.open(i) for i in queries]
 
-        if type(image) == str:
-            img = Image.open(image, mode='RGB')
-        return F.crop(img, bbox[1], bbox[0], bbox[3], bbox[2])
-    
     def __len__(self):
         return len(self.keys)
     
@@ -91,116 +176,27 @@ class CocoBoxes(Dataset):
         if torch.is_tensor(index):
             index = index.tolist()
         
-        img_path = self.id_to_file(self.keys[index])
-        bboxes = self.image_ids(self.keys[index])
+        img_path = id_to_file(self.keys[index], self.image_dir)
+        bboxes = self.image_ids[self.keys[index]]
         img_class: int = random.choices(list(bboxes.keys()), k=1)[0]  # randomly chose an image class to preform detection
 
-        img = Image.open(img_path, 'RBG')  # the target image
+        img = Image.open(img_path)  # the target image
         bboxes = np.asarray(bboxes[img_class])  # all the boxes in that class belonging to the target image
+
         if self.transforms is not None:
             img, bboxes = self.transforms(img, bboxes)
         
-        # TODO: make case4 work
-        queries, bboxes = self.random_query_bbox(bboxes)
-        queries = self.fetch_query(img, queries)
+        # get queries
+        if self.query_pool is not None and random.random() < self.case4_prob:
+            # queries are pulled from disk at location self.query_pool
+            queries = self.random_query(img_class, self.case4_sigma)
+        else:
+            # queries are pulled from the target image, and are a subset of bboxes
+            queries, bboxes = self.random_query_bbox(bboxes)
+            queries = [fetch_query(img, q) for q in queries]
+        
         if self.query_transforms is not None:
             queries = self.query_transforms(queries)
-        
+
         return img, queries, bboxes
 
-
-class CocoDetection(torchvision.datasets.CocoDetection):
-    def __init__(self, img_folder, ann_file, transforms, return_masks):
-        super(CocoDetection, self).__init__(img_folder, ann_file)
-        self._transforms = transforms
-        self.prepare = ConvertCocoPolysToMask(return_masks)
-
-    def __getitem__(self, idx):
-        img, target = super(CocoDetection, self).__getitem__(idx)
-        image_id = self.ids[idx]
-        target = {'image_id': image_id, 'annotations': target}
-        img, target = self.prepare(img, target)
-        if self._transforms is not None:
-            img, target = self._transforms(img, target)
-        return img, target
-
-def convert_coco_poly_to_mask(segmentations, height, width):
-    masks = []
-    for polygons in segmentations:
-        rles = coco_mask.frPyObjects(polygons, height, width)
-        mask = coco_mask.decode(rles)
-        if len(mask.shape) < 3:
-            mask = mask[..., None]
-        mask = torch.as_tensor(mask, dtype=torch.uint8)
-        mask = mask.any(dim=2)
-        masks.append(mask)
-    if masks:
-        masks = torch.stack(masks, dim=0)
-    else:
-        masks = torch.zeros((0, height, width), dtype=torch.uint8)
-    return masks
-
-
-class ConvertCocoPolysToMask(object):
-    def __init__(self, return_masks=False):
-        self.return_masks = return_masks
-
-    def __call__(self, image, target):
-        w, h = image.size
-
-        image_id = target["image_id"]
-        image_id = torch.tensor([image_id])
-
-        anno = target["annotations"]
-
-        anno = [obj for obj in anno if 'iscrowd' not in obj or obj['iscrowd'] == 0]
-
-        boxes = [obj["bbox"] for obj in anno]
-        # guard against no boxes via resizing
-        boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
-        boxes[:, 2:] += boxes[:, :2]
-        boxes[:, 0::2].clamp_(min=0, max=w)
-        boxes[:, 1::2].clamp_(min=0, max=h)
-
-        classes = [obj["category_id"] for obj in anno]
-        classes = torch.tensor(classes, dtype=torch.int64)
-
-        if self.return_masks:
-            segmentations = [obj["segmentation"] for obj in anno]
-            masks = convert_coco_poly_to_mask(segmentations, h, w)
-
-        keypoints = None
-        if anno and "keypoints" in anno[0]:
-            keypoints = [obj["keypoints"] for obj in anno]
-            keypoints = torch.as_tensor(keypoints, dtype=torch.float32)
-            num_keypoints = keypoints.shape[0]
-            if num_keypoints:
-                keypoints = keypoints.view(num_keypoints, -1, 3)
-
-        keep = (boxes[:, 3] > boxes[:, 1]) & (boxes[:, 2] > boxes[:, 0])
-        boxes = boxes[keep]
-        classes = classes[keep]
-        if self.return_masks:
-            masks = masks[keep]
-        if keypoints is not None:
-            keypoints = keypoints[keep]
-
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = classes
-        if self.return_masks:
-            target["masks"] = masks
-        target["image_id"] = image_id
-        if keypoints is not None:
-            target["keypoints"] = keypoints
-
-        # for conversion to coco api
-        area = torch.tensor([obj["area"] for obj in anno])
-        iscrowd = torch.tensor([obj["iscrowd"] if "iscrowd" in obj else 0 for obj in anno])
-        target["area"] = area[keep]
-        target["iscrowd"] = iscrowd[keep]
-
-        target["orig_size"] = torch.as_tensor([int(h), int(w)])
-        target["size"] = torch.as_tensor([int(h), int(w)])
-
-        return image, target
