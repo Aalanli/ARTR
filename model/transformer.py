5ce8426eb61f1f88c8cdd8ef70a41d18928940be
AL <allanlin666666@gmail.com>
2021-08-13 17:33:38 -0700

    added 1d positional embedding

--- model/transformer.py
+++ model/transformer.py
@@ -46,6 +46,19 @@ class PositionEmbeddingSine(nn.Module):
         return pos
 
 
+class FixedPositionalEmbedding(nn.Module):
+    def __init__(self, dim, max_seq_len):
+        super().__init__()
+        inv_freq = 1. / (10000 ** (torch.arange(0, dim, 2).float() / dim))
+        position = torch.arange(0, max_seq_len, dtype=torch.float)
+        sinusoid_inp = torch.einsum("i,j->ij", position, inv_freq)
+        emb = torch.cat((sinusoid_inp.sin(), sinusoid_inp.cos()), dim=-1)
+        self.register_buffer('emb', emb)
+
+    def forward(self, x):
+        return self.emb[None, :x.shape[1], :].to(x)
+
+
 # %%
 def split_heads(x: torch.Tensor, heads: int):
     # shape = [batch, sequence, features]
