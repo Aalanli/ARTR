d2eaef75bdc3abb132cd8033e00acd6f8b5edc7a
AL <allanlin666666@gmail.com>
2021-08-24 17:24:30 -0700

    Refactored model to integrate memory efficient pad + concat

--- model/baseline/model.py
+++ model/baseline/model.py
@@ -1,14 +1,14 @@
 # %%
-from typing import List
+from typing import Dict, List, Tuple
 
 import torch
-from torch._C import dtype
 import torch.nn.functional as F
 from torch import nn
 
-from model.transformer import EncoderLayer, DecoderLayer, PositionEmbeddingSine
+from model.transformer import EncoderLayer, DecoderLayer, PositionEmbeddingSineMaskless
 from model.resnet_parts import Backbone
-from utils.ops import nested_tensor_from_tensor_list, max_by_axis
+from utils.misc import make_equal, make_equal1D
+
 
 class Transformer(nn.Module):
     def __init__(
@@ -32,16 +32,20 @@ class Transformer(nn.Module):
         self.dec_final = nn.ModuleList([DecoderLayer(*args) for _ in range(dec_nlayers)])
     
     def forward(self, im_query, im_target, query_embed, mask_q=None, mask_t=None, pos_q=None, pos_t=None):
+        key_padding_mask_q = mask_q[:, None, None, :].bool()
+        key_padding_mask_t = mask_t[:, None, None, :].bool()
         for layer in self.enc_q:
-            im_query = layer(im_query, mask_q, pos_q)
+            im_query = layer(im_query, key_padding_mask_q, pos_q)
         for layer in self.enc_t:
-            im_target = layer(im_target, mask_t, pos_t)
+            im_target = layer(im_target, key_padding_mask_t, pos_t)
 
+        attn_2d_mask = mask_q[:, :, None].logical_or(mask_t[:, None, :])
+        attn_2d_mask.unsqueeze_(1)         # [batch, 1, L, D]
         x = torch.zeros_like(query_embed)  # flow-through variable
         kv = torch.zeros_like(im_query)    # flow-through variable
         for kv_layer, out_layer in zip(self.dec_kv, self.dec_final):
-            kv = kv_layer(kv, im_query, im_target, mask_t, pos_t)
-            x = out_layer(x, query_embed, kv, mask_q, pos_q)
+            kv = kv_layer(kv, im_query, im_target, attn_2d_mask, pos_t)
+            x = out_layer(x, query_embed, kv, key_padding_mask_q, pos_q)
         return x
 
 
@@ -61,73 +65,105 @@ class MLP(nn.Module):
 
 
 class ARTR(nn.Module):
-    def __init__(self, backbone_args: List, pos_embed_args: List, transformer_args: List, num_queries: int):
+    def __init__(
+        self, 
+        backbone_args:    Dict = {'name': 'resnet50', 'train_backbone': True, 'return_interm_layers': False, 'dilation': False}, 
+        pos_embed_args:   Dict = {'num_pos_feats': 128, 'temperature': 10000, 'normalize': False, 'scale': None}, 
+        transformer_args: Dict = {'d_model': 256, 'heads': 8, 'proj_forward': 1024, 'enc_q_nlayers': 3,
+                                  'enc_t_nlayers': 3, 'dec_nlayers': 3, 'activation': F.relu, 'dropout': 0.1, 'bias': None}, 
+        num_queries:      int  = 50
+        ):
         super().__init__()
-        self.config_ = [backbone_args, pos_embed_args, transformer_args, num_queries]
+        # default args
+        backbone_args_ = {'name': 'resnet50', 'train_backbone': True, 'return_interm_layers': False, 'dilation': False}
+        pos_embed_args_ = {'num_pos_feats': 128, 'temperature': 10000, 'normalize': False, 'scale': None}
+        transformer_args_ = {'d_model': 256, 'heads': 8, 'proj_forward': 1024, 'enc_q_nlayers': 3,
+                             'enc_t_nlayers': 3, 'dec_nlayers': 3, 'activation': F.relu, 'dropout': 0.1, 'bias': None}
+        backbone_args_.update(backbone_args)
+        pos_embed_args_.update(pos_embed_args)
+        transformer_args_.update(transformer_args)
+        self.config_ = [backbone_args_, pos_embed_args_, transformer_args_, num_queries]
 
-        self.backbone = Backbone(*backbone_args)
-        self.pos_embed = PositionEmbeddingSine(*pos_embed_args)
-        self.transformer = Transformer(*transformer_args)
+        self.backbone = Backbone(**backbone_args_)
+        self.pos_embed = PositionEmbeddingSineMaskless(**pos_embed_args_)
+        self.transformer = Transformer(**transformer_args_)
         d_model = self.transformer.d_model
 
         self.class_embed = nn.Linear(d_model, 2)
         self.bbox_embed = MLP(d_model, d_model, 4, 3)
         self.query_embed = nn.Parameter(torch.Tensor(num_queries, d_model))
         torch.nn.init.normal_(self.query_embed)
-        self.query_proj = nn.Parameter(torch.Tensor(num_queries, num_queries))
-        torch.nn.init.normal_(self.query_proj)
+        self.input_proj = nn.Conv2d(self.backbone.num_channels, self.transformer.d_model, kernel_size=1)
+        
         self.input_proj = nn.Conv2d(self.backbone.num_channels, d_model, kernel_size=1)
     
-    def forward(self, tar_im: torch.Tensor, query_im: List[torch.Tensor], dist: torch.Tensor, t_mask: torch.Tensor=None, query_mask: List[torch.Tensor]=None) -> torch.Tensor:
+    def make_equal_masks(self, ims: List[torch.Tensor]):
+        """Accepts a list of flattened images and produces binary masks for attention"""
+        shapes = [i.shape[-1] for i in ims]
+        batch = len(ims)
+        max_len = max(shapes)
+        mask = torch.zeros(batch, max_len, dtype=ims[0].dtype, device=ims[0].device)
+        for i, s in enumerate(shapes):
+            mask[i, s:].fill_(1)
+        return mask.bool()
+    
+    def make_pos_embed(self, x: List[torch.Tensor]):
+        """Makes a 2D positional embedding from a 2D image"""
+        # [embed_dim, x * y]
+        return self.pos_embed(x).permute(2, 0, 1).flatten(1)
+
+    def make_equal_ims(self, ims: List[torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Unrolls resnet features and calculates masks and positional embed,
+        vectorizes all.
+        """
+        pos_embed = []
+        for i in range(len(ims)):
+            ims[i] = self.backbone(ims[i].unsqueeze_(0))['0']
+            ims[i] = self.input_proj(ims[i]).squeeze()
+            pos_embed.append(self.make_pos_embed(ims[i]))
+            ims[i] = ims[i].flatten(1)
+        pos_embed = make_equal1D(pos_embed)  # no grad
+        masks = self.make_equal_masks(ims)   # no grad
+        ims = make_equal(*ims)               # with grad
+        return ims, masks, pos_embed
+    
+    def make_equal_queries(self, qrs: List[List[torch.Tensor]]) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Same as make_equal_ims method, but unrolls multiple queries into
+        one for each sample in a batch
+        """
+        pos_embed = []
+        for j in range(len(qrs)):
+            pos_embed_ = []
+            for i in range(len(qrs[j])):
+                qrs[j][i] = self.backbone(qrs[j][i].unsqueeze_(0))['0']
+                qrs[j][i] = self.input_proj(qrs[j][i]).squeeze()
+                pos_embed_.append(self.make_pos_embed(qrs[j][i]))
+                qrs[j][i] = qrs[j][i].flatten(1)
+            # [embed_dim, n * x * y]
+            pos_embed.append(torch.cat(pos_embed_, dim=-1))
+            qrs[j] = torch.cat(qrs[j], dim=-1)
+        pos_embed = make_equal1D(pos_embed)
+        masks = self.make_equal_masks(qrs)
+        qrs = make_equal(*qrs)
+        return qrs, masks, pos_embed
+    
+    def forward(self, tar_im: List[torch.Tensor], query_im: List[List[torch.Tensor]]):
         """
         len(tar_im) = batch_size, len(query_im) = batch_size, len(query_im[i]) = # of query images for tar_im[i]
         dist.shape = [batch_size]; a scaler difference between the bounding boxes and query images 
         """
-        if isinstance(tar_im, (list, torch.Tensor)):
-            tar_im, t_mask = nested_tensor_from_tensor_list(tar_im)
-        t_features, t_mask = self.backbone(tar_im, t_mask)['0']   # [batch, num_channels, x, y]
-        t_features = self.input_proj(t_features)                  # [batch, d_model, x, y]
-        t_pos = self.pos_embed(t_mask)                            # [batch, x, y, d_model]
-        t_features = t_features.flatten(2).transpose(-1, -2)      # [batch, x*y, d_model]
-        t_pos = t_pos.reshape(list(t_features.shape))             # [batch, x*y, d_model]
-        t_mask = t_mask.flatten(1)                                # [batch, x*y]
-        
-        q_features = []
-        q_mask = []
-        q_pos = []
-        for i, q in enumerate(query_im):
-            # treat each query as a batch
-            if isinstance(tar_im, (list, torch.Tensor)):
-                qf, qm = nested_tensor_from_tensor_list(q)
-            else:
-                qf, qm = q, query_mask[i]
-            qf, qm = self.backbone(qf, qm)['0']                   # [n, 3, x1, y1], [n, x1, y1]
-            qf = self.input_proj(qf).transpose(1, 0).flatten(1)   # [d_model, n*x1*y1]
-            qpos = self.pos_embed(qm).transpose(-1, 0).flatten(1) # [d_model, n*x1*y1]
-            qm = qm.flatten(0)                                    # [n*x1*y1]
-            q_features.append(qf)
-            q_mask.append(qm)
-            q_pos.append(qpos)
-        max_len = max([i.shape[0] for i in q_mask])
-        for b in range(len(q_mask)):
-            # pad across aggregated samples
-            # pad with zeros
-            q_features[b] = F.pad(q_features[b], (0, max_len - q_features[b].shape[-1]))
-            q_pos[b] = F.pad(q_pos[b], (0, max_len - q_pos[b].shape[-1]))
-            # pad with ones
-            q_mask[b] = torch.cat([q_mask[b], torch.ones(max_len - q_mask[b].shape[-1], dtype=torch.bool, device=q_mask[b].device)])
-        
-        q_features, q_mask, q_pos = map(lambda x: torch.stack(x), (q_features, q_mask, q_pos))
+        t_features, t_mask, t_pos = self.make_equal_ims(tar_im)
+        q_features, q_mask, q_pos = self.make_equal_queries(query_im)
+        t_features = t_features.transpose(-1, -2)
+        q_features = q_features.transpose(-1, -2)
+        t_pos.transpose_(-1, -2)
+        q_pos.transpose_(-1, -2)
 
-        q_features = q_features.transpose(-1, -2)                 # [batch, n*x1*y1, d_model]
-        q_pos = q_pos.transpose(-1, -2)                           # [batch, n*x1*y1, d_model]
-
-        # transform the queries by the difference scalar
-        query_proj = (self.query_proj.unsqueeze(-1) * (dist + 1e-6)).transpose(-1, 0)  # [batch, num_queries, num_queries]
-        query_embed = query_proj @ self.query_embed                                    # [batch, num_queries, d_model]
-
+        batch_size = t_pos.shape[0]
+        query_embed = self.query_embed.unsqueeze(0).repeat(batch_size, 1, 1)
         out = self.transformer(q_features, t_features, query_embed, q_mask, t_mask, q_pos, t_pos)
-
         output_class = self.class_embed(out)
         output_bbox = self.bbox_embed(out).sigmoid()
         return {'pred_logits': output_class, 'pred_boxes': output_bbox[-1]}
@@ -135,5 +171,5 @@ class ARTR(nn.Module):
     @property
     def config(self):
         return self.config_
-    
+
 
